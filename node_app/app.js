85af3c2d05f56624c99b048f39a58363d43700b1
thanhloc1506 <thanhloc1506@gmail.com>
2020-10-01 16:21:44 +0700

    Check array note

--- node_app/app.js
+++ node_app/app.js
@@ -1,12 +1,10 @@
 const fs = require("fs");
 
-const lib = require("./lib")
+const lib = require("./lib");
 const outputDir = "output";
 const filename = "Hello.txt";
 const content = "Hello Kmin 2";
 const filePath = outputDir + "/" + filename;
-
-
 
 //tmp1.writeContent("output", "hello.txt", "Hello World 1");
 //tmp1.deleteContent('output/Hello.txt');
@@ -29,14 +27,8 @@ const filePath = outputDir + "/" + filename;
 
 // console.log(`Writen "${content}" to file "${filename}"`)
 
-
-
-
-
-
 // console.log('Written "%s" to file "%s"', content, filename);
 // console.log(`Written "` + content + '" to file "' + filename +'"');
-
 
 // delete a file
 // fs.unlink('Hello.txt', (err) => {
@@ -44,44 +36,42 @@ const filePath = outputDir + "/" + filename;
 //         throw err;
 //     }
 
-    // console.log("\x1b[34m", "File is deleted.");
-     const chalk = require('chalk');
- 
-console.log(chalk.blue('Hello world 111 !'));
+// console.log("\x1b[34m", "File is deleted.");
+const chalk = require("chalk");
+
+console.log(chalk.blue("Hello world 111 !"));
 //console.log(process.argv[2]);
-const yargs = require('yargs');
-const argv  = require("process");
+const yargs = require("yargs");
+const argv = require("process");
 
-yargs.version('1.1.0')
+yargs.version("1.1.0");
 
-yargs.command({ 
-     command: 'add',
-     describe: 'Add a new note', 
-     builder: {
-       title: {
-         describe: 'Note title', 
-         demandOption: true, 
-         type: 'string'
-       }, 
-       description: {
-         describe: 'Note description', 
-         demandOption: true, 
-         type: 'string'
-       }
-     },
-     handler: function (args) { 
-          const {title, description} = args;
-       console.log('Title: ' + title); 
-       console.log('Description: ' + description);
-       const noteAdd = {
-        title: "",
-        description: "" 
-       }
-       noteAdd.title = title;
-       noteAdd.description = description;
-       const Anotes = lib.load('note.json');
-       Anotes.push(noteAdd);
-       lib.saveNote('note.json', Anote);
-     } 
-   }).argv;
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    description: {
+      describe: "Note description",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (args) {
+    const { title, description } = args;
+    console.log("Title: " + title);
+    console.log("Description: " + description);
+    const noteAdd = {title, description};
+
+    const Arrnotes = lib.load('note.json');
+    if(lib.checkNote(Arrnotes, noteAdd)){
+      Arrnotes.push(noteAdd);
+    }
+    lib.saveNote('note.json', Arrnotes);
+  },
+}).argv;
 // });
--- node_app/lib.js
+++ node_app/lib.js
@@ -25,19 +25,32 @@ function load(fileName){
     try{
         const nodtesJson = fs.readFileSync(fileName, {encoding: "utf-8"})
         return JSON.parse(nodtesJson);
-    }catch{
+    }catch (error){
         return [];
     }
 }
 
 function saveNote(fileName, notes){
     const noteJson = JSON.stringify(notes);
-    fs.writeFileSync(fileName, notes);
+    fs.writeFileSync(fileName, noteJson);
 }
+
+function checkNote(arrNote, newNote){
+    for (const property in arrNote) {
+        //console.log(arrNote[`${property}`].title === newNote.title);
+        if(arrNote[`${property}`].title === newNote.title && arrNote[`${property}`].description === newNote.description){
+            console.log('ok');
+            return false;
+        }
+      }
+    return true;
+}
+
 
 module.exports = {
     writeContent,
     deleteContent,
     load,
-    saveNote
+    saveNote,
+    checkNote
 }
