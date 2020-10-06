c57eb60bd4aefb94684d73a80af432c136bb7388
thanhloc1506 <thanhloc1506@gmail.com>
2020-10-06 14:10:55 +0700

    Refactor code

--- node_app/app.js
+++ node_app/app.js
@@ -8,39 +8,14 @@ const filePath = outputDir + "/" + filename;
 
 //tmp1.writeContent("output", "hello.txt", "Hello World 1");
 //tmp1.deleteContent('output/Hello.txt');
-// fs.access("./output", function(error) {
-//     if (error) {
-
-//         fs.mkdir("./output", function(err) {
-//             if (err) {
-//               console.log(err)
-//             } else {
-//               console.log("New directory successfully created.")
-//             }
-//           })
-
-//         fs.writeFileSync(filePath, content);
-//     } else {
-//       console.log("Directory exists.")
-//     }
-//   })
 
 // console.log(`Writen "${content}" to file "${filename}"`)
-
 // console.log('Written "%s" to file "%s"', content, filename);
 // console.log(`Written "` + content + '" to file "' + filename +'"');
 
-// delete a file
-// fs.unlink('Hello.txt', (err) => {
-//     if (err) {
-//         throw err;
-//     }
-
-// console.log("\x1b[34m", "File is deleted.");
 const chalk = require("chalk");
 
 console.log(chalk.blue("Hello world 111 !"));
-//console.log(process.argv[2]);
 const yargs = require("yargs");
 const argv = require("process");
 
@@ -98,5 +73,5 @@ yargs
     },
   })
   .alias("list", "ls").argv;
-
+//
 // });
