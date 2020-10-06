3ae8da4b56f0214e7e5bfa6ee9e0544e86eb8185
thanhloc1506 <thanhloc1506@gmail.com>
2020-10-06 14:53:38 +0700

    Refactor code

--- playground/Json.js
+++ playground/Json.js
@@ -11,25 +11,17 @@ const bookJSON = JSON.stringify(book)
 
 // Covert JSON string into object
 const bookObject = JSON.parse(bookJSON) 
-
-// console.log(bookJSON)
-// console.log(bookObject)
-// console.log(bookObject.title)
-// console.log(bookObject.author)
-
-//fs.writeFileSync("book.json", JSON.stringify(book));
-
 const bookContent = fs.readFileSync("book.json", {encoding: "utf-8"});
 const bookcont = JSON.parse(bookContent);
+
 //console.log(bookcont.title);
 const yargs = require('yargs');
 const argv  = require("process");
 
 yargs.version('1.1.0')
-
 yargs.command({ 
 	command: 'write',
-	handler: function (args) { 
+	handler: (args) => { 
 		const {nameDir, fileName} = args;
 		lib.writeContent(nameDir, fileName, bookcont.title);
 	} 
